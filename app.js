5e450188de0fb3a777cea1b3da65ef72fd7b255e
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-09 18:52:08 +0700

    setting up get and post conn and body parsing obj

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
 const connect = require("./config/db_connect");
+const bodyParser = require("body-parser");
+const checkURL = require("./models/url");
 
 // Connect to database
 connect();
+
+// Parsing the data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// get data
+
+app.get("/", (req, res) => {
+  res.send("Your data here");
+});
+
+app.post("/", async (req, res) => {
+  console.log(`form data: ${req.body}`);
+  const { url, customSlug } = req.body;
+
+  const result = await checkURL.find({ customSlug: customSlug });
+  console.log(result);
+});
 
 const port = 8000;
 
