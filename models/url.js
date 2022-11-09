2e3a74d7a58a62603d518e3ecbbf78e3fb4f048f
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-09 13:22:48 +0700

    create url file for database schema

---
+++ models/url.js
@@ -0,0 +1,25 @@
+const mongoose = require(mongoose);
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    customSlug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    resultUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps,
+  }
+);
+
+module.exports = mongoose.model("url", urlSchema);
