b9a1396d6395e9a2f65599f099616a54f76c8c79
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-11 09:39:38 +0700

    Changing username and password with env var

--- config/key.js
+++ config/key.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://numanarif:akunumanarif@cluster0.ecx0a.mongodb.net/Shortify?retryWrites=true&w=majority",
+    "mongodb+srv://" +
+    process.env.MONGO_URI_USERNAME +
+    ":" +
+    process.env.MONGO_URI_PASSWORD +
+    "@cluster0.ecx0a.mongodb.net/Shortify?retryWrites=true&w=majority",
 };
